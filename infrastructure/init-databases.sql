1f04c3c928c43857a6f1e7e0032d02d5eb2f2c7c
groupcpoc <groupcpoc2026@gmail.com>
2026-07-08 17:09:20 +0530

    Revert "Feature/inventory service"

--- infrastructure/init-databases.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE ecommerce_auth_db;
-CREATE DATABASE ecommerce_user_db;
-CREATE DATABASE ecommerce_product_db;
-CREATE DATABASE ecommerce_order_db;
-CREATE DATABASE ecommerce_payment_db;
-CREATE DATABASE ecommerce_inventory_db;
-CREATE DATABASE ecommerce_notification_db;
-CREATE DATABASE ecommerce_keycloak_db;
